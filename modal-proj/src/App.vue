69104a6f6b023c026ea9efc6cbea8e889b14e210
hothoony <hothoony@hanmail.net>
2022-03-25 19:58:04 +0900

    .

---
+++ modal-proj/src/App.vue
@@ -0,0 +1,68 @@
+<template>
+  <h1>hello vue.js</h1>
+  <h1>{{ title }}</h1>
+
+  <teleport to="#modals" v-if="showModal">
+    <Modal theme="" @close="toggleModal">
+      <template v-slot:links>
+        <a href="#">sign up</a>
+        <a href="#">more info</a>
+      </template>
+      <h1>modal1 title</h1>
+      <p>modal1 content</p>
+    </Modal>
+  </teleport>
+
+  <teleport to="#modals" v-if="showModal2">
+    <Modal theme="" @close="toggleModal2">
+      <h1>modal2 title</h1>
+      <p>modal2 content</p>
+    </Modal>
+  </teleport>
+
+  <button @click="toggleModal">show modal 1</button>
+  <button @click="toggleModal2">show modal 2</button>
+
+</template>
+
+<script>
+import Modal from './components/Modal.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Modal
+  },
+  data() {
+    return {
+      title: 'My First Vue App :)',
+      header: 'title 22',
+      message: 'content 22',
+      showModal: false,
+      showModal2: false,
+    }
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
+    toggleModal2() {
+      this.showModal2 = !this.showModal2;
+    },
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1 {
+  color: red;
+}
+</style>
---
+++ modal-proj/src/components/Modal.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="backdrop" @click="closeModal">
+        <div class="modal" :class="{ sale: theme === 'sale' }">
+            <slot></slot>
+            <!-- <div class="actions"> -->
+                <slot name="links"></slot>
+            <!-- </div> -->
+        </div>
+    </div>    
+</template>
+
+<script>
+export default {
+    props: ['header', 'message', 'theme'],
+    methods: {
+        closeModal() {
+            this.$emit('close');
+        }
+    }
+}
+</script>
+
+
+<style>
+.modal {
+    width: 400px;
+    padding: 20px;
+    margin: 100px auto;
+    background: white;
+    border-radius: 10px;
+}
+.backdrop {
+    top: 0;
+    position: fixed;
+    background: rgba(0, 0, 0, 0.5);
+    width: 100%;
+    height: 100%;
+}
+.sale {
+    color: orange;
+}
+</style>
