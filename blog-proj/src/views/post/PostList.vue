1256dd2e20c3b48204e6bf7312b27b6115aa126c
hothoony <hothoony@hanmail.net>
2022-03-31 20:23:59 +0900

    .

--- blog-proj/src/views/post/PostList.vue
+++ blog-proj/src/views/post/PostList.vue
@@ -3,20 +3,30 @@
       <p>PostList</p>
       <div v-for='post in posts' :key='post.id'>
           <PostView :post='post' />
+          <button>toggle posts</button>
       </div>
   </div>
 </template>
 
 <script>
+import { onMounted } from '@vue/runtime-core';
 import PostView from './PostView.vue'
 
 export default {
     props: ['posts'],
-    components: {
-        PostView
-    },
+    components: { PostView },
     setup(props) {
         console.log('PostList', props.posts);
+
+        onMounted(() => {
+            console.log('mounted');
+        });
+        // onUnmounted(() => {
+        //     console.log('onUnmounted');
+        // });
+        // onUpdated(() => {
+        //     console.log('onUpdated');
+        // });
     },
 }
 </script>
--- blog-proj/src/views/post/PostView.vue
+++ blog-proj/src/views/post/PostView.vue
@@ -1,13 +1,21 @@
 <template>
   <!-- <p>PostView</p> -->
   <p>PostView : {{ post.title }} - {{ post.content }}</p>
+  <p>{{ snippet }}</p>
 </template>
 
 <script>
+import { computed } from '@vue/runtime-core';
 export default {
     props: ['post'],
     setup(props) {
         console.log('PostView', props.post);
+        const snippet = computed(() => {
+          console.log('snippet');
+          return props.post.content.substring(0, 5) + '...';
+        });
+
+        return { snippet };
     }
 }
 </script>
